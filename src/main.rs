3037f17934d863141bd2a01767ae0537d82f002e
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-03-23 16:50:58 +0000

    Tokenising things in preparation for adding variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::io;
 use std::io::Write;
 use std::process::exit;
@@ -5,17 +6,61 @@ use std::process::exit;
 #[derive(Debug,PartialEq,Clone,Copy)]
 enum TokenType {
     Num,
+    Strng,
     BinaryOp,
     Keyword,
     Error,
 }
 
-type Stack = Vec<f64>;
+struct Token {
+    token_type: TokenType,
+    value: f64,
+    text: String,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.token_type {
+            TokenType::Num => write!(f, "{}", self.value),
+            TokenType::Strng => write!(f, "{}", self.text),
+            _ => unreachable!("Trying to disp a non-string/value"),
+        }
+    }
+}
+
+struct Stack(pub Vec<Token>);
+
+impl Stack {
+    fn push(&mut self, item: Token) {
+        self.0.push(item);
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn pop(&mut self) -> Option<Token> {
+        self.0.pop()
+    }
+
+    fn clear(&mut self) {
+        self.0.clear()
+    }
+}
+
+impl fmt::Display for Stack {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for tok in &self.0 {
+            write!(f, "    {}\n", tok);
+        }
+        Ok(())
+    }
+}
 
 fn lex(text: &str) -> TokenType {
     match text {
         "+" | "-" | "*" | "/" | "==" => TokenType::BinaryOp,
-        "clear" | "reset" | "exit" => TokenType::Keyword,
+        "clear" | "reset" | "exit" | "print" => TokenType::Keyword,
         _ => {
             if text.parse::<f64>().is_ok() {
                 TokenType::Num
@@ -31,22 +76,42 @@ fn parse_input(text: &str, mut stack: Stack) -> Stack {
     for item in text.split_whitespace() {
         match lex(item) {
             TokenType::Error => break,
-            TokenType::Num => {stack.push(item.parse::<f64>().unwrap())},
+            TokenType::Num => {
+                let tok = Token{
+                    token_type: TokenType::Num,
+                    value: item.parse::<f64>().unwrap(),
+                    text: "".to_string(),
+                };
+                stack.push(tok);
+            }
+            TokenType::Strng => {
+                let tok = Token{
+                    token_type: TokenType::Strng,
+                    value: 0f64,
+                    text: item.to_string(),
+                };
+                stack.push(tok);
+            },
             TokenType::BinaryOp => {
                 if stack.len() >= 2 {
                     let b = stack.pop().unwrap();
                     let a = stack.pop().unwrap();
                     if let Some(result) = match item {
-                        "+" => Some(a + b),
-                        "-" => Some(a - b),
-                        "*" => Some(a * b),
-                        "/" => Some(a / b),
+                        "+" => Some(a.value + b.value),
+                        "-" => Some(a.value - b.value),
+                        "*" => Some(a.value * b.value),
+                        "/" => Some(a.value / b.value),
                         _ => {
                             println!("Unknown binary op: {}", item);
                             None
                         }
                     } {
-                        stack.push(result);
+                        let tok = Token{
+                            token_type: TokenType::Num,
+                            value: result,
+                            text: "".to_string(),
+                        };
+                        stack.push(tok);
                     }
                 } else {
                     println!("Insufficient values on stack for binary operation");
@@ -56,6 +121,14 @@ fn parse_input(text: &str, mut stack: Stack) -> Stack {
                 match item {
                     "clear" | "reset" => stack.clear(),
                     "exit" => exit(0),
+                    "print" => {
+                        if stack.len() > 0 {
+                            let val = stack.pop().unwrap();
+                            println!("{}", val);
+                        } else {
+                            println!("Nothing on the stack to print.");
+                        }
+                    },
                     _ => println!("Unknown keyword: {}", item),
                 }
             }
@@ -68,7 +141,7 @@ fn parse_input(text: &str, mut stack: Stack) -> Stack {
 fn main() {
     let mut stdout = io::stdout();
     let stdin = io::stdin();
-    let mut stack: Stack = vec![];
+    let mut stack: Stack = Stack(vec![]);
     println!("RustPN: A Rust powered RPN calculator.");
 
     loop {
@@ -78,6 +151,6 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
 
         stack = parse_input(&input, stack);
-        println!("Stack (len={}): {:#?}", stack.len(), stack);
+        println!("Stack (len={}):\n{}", stack.len(), stack);
     }
 }
